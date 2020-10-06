a8269dcdb8b85f267ee7a6d5a5728ff9adbcb147
Marcos_Vinici10 <viniciusfigueiredo@ucl.br>
2020-10-06 22:56:56 +0000

    NEWV

--- Mostruario.cs
+++ Mostruario.cs
@@ -1,6 +1,19 @@
+using System.Collections.Generic;
+
 class Mostruario {
   //TAD
-  Vendedor Representante;
+  private Vendedor Representante;
+        //EST  <TAD>    VARIAVEL 
+  public List<Veiculo> carros;
+
+  public Mostruario(){
+    carros = new List<Veiculo>();
+
+  }
+
+  public void setRepresentante(Vendedor v){
+    Representante = v;
+  }
 
 
 }
--- main.cs
+++ main.cs
@@ -2,6 +2,15 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Vendedor carlos = new Vendedor();
+    Veiculo uno = new Veiculo();
+
+    Mostruario secaoA = new Mostruario();
+    secaoA.setRepresentante(carlos);
+
+    secaoA.carros.Add(uno);
+
+
+
   }
 }
